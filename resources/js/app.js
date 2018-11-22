3394b6a6499d832852f93652905c2e9850ed59d2
Your Name <thanh@eocenterprise.com>
2018-11-22 22:47:30 +0700

    done

--- resources/js/app.js
+++ resources/js/app.js
@@ -32,5 +32,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+   	template: '<app></app>',
+    components: { App },
+    router
 });
